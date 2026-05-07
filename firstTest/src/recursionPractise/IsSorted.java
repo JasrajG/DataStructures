d4a970daa394070a5d08c0777fde39fd9bdac78d
Gill <jasrajgill1@hotmail.com>
2026-05-07 07:23:51 -0400

    Created isSorted
    isSorted just takes an integer array and sees if it is sorted or not.

---
+++ firstTest/src/recursionPractise/IsSorted.java
@@ -0,0 +1,34 @@
+package recursionPractise;
+
+public class IsSorted {
+
+	
+	//initializer
+	public static boolean isSortedM(int[] arr) {
+		
+		int i=0; //counter
+		
+		return isSortedRec(i, arr);
+	
+	}
+	
+	
+	//recursive method
+	//this method assumes that the values inside the array have been initilalized
+	public static boolean isSortedRec(int i, int[] arr) {
+		if (i == arr.length -1) {
+			return true;
+		}
+		else if(arr[i] > arr[i+1]) {
+			return false;
+		}
+		
+		else {
+			return isSortedRec(i+1, arr);
+		}
+		
+		
+	}
+	
+	
+}
