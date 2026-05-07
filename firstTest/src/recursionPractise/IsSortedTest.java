71400ecc0dca00a996dc742b511c0c1fadf6a98e
Gill <jasrajgill1@hotmail.com>
2026-05-07 07:36:42 -0400

    Created Junit Tests for isSorted
    just one

---
+++ firstTest/src/recursionPractise/IsSortedTest.java
@@ -0,0 +1,18 @@
+package recursionPractise;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class IsSortedTest {
+
+	@Test
+	void test1() {
+		
+		int[] array = {1,2,3,4,5,6,7,8,8,10};
+		
+		assertTrue(IsSorted.isSortedM(array));
+		
+	}
+
+}
