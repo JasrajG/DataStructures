6f790a17d7253c793086974b00829d147bc3b643
Gill <jasrajgill1@hotmail.com>
2026-05-08 20:28:16 -0400

    Implemented Tower of Hanoi Problem
    Didn't include letters for the discs though might do that later

--- firstTest/src/firstTest/Tester.java
+++ firstTest/src/firstTest/Tester.java
@@ -3,7 +3,9 @@ package firstTest;
 public class Tester {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
+		int i = 1;
+		System.out.println("Hello World!" + i);
 
 	}
 
---
+++ firstTest/src/recursionPractise/TowerOfHanoiClass.java
@@ -0,0 +1,43 @@
+package recursionPractise;
+
+public class TowerOfHanoiClass {
+
+	
+	public static void TofH(int n) {
+		
+		int initial = 1;
+		int intermediate = 2; //intermediate
+		int destination = 3; //destination
+		
+		TofHRec(n,initial, intermediate,destination);
+		
+	}
+	
+	public static void TofHRec(int n, int initial, int intermediate, int destination) {
+		
+		//base case
+		
+		if (n==1) 
+		{
+			System.out.println("Move from peg" + initial +" to peg" + destination);
+			return;
+			}
+		//recrusive step 1
+		TofHRec(n-1, initial, destination, intermediate);
+		
+		
+		//recursive step 2
+		System.out.println("Move from peg" + initial +" to peg" + destination);
+		
+		
+		//recursive step 3
+		TofHRec(n-1, intermediate, initial, destination);
+		
+		
+	}
+	
+	public static void main(String args[]) {
+		TofH(64);
+	}
+	
+}
